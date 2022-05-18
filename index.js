17e0ba956ebe0ec2231ed8fb0213f546d4646e18
John Garbero <john.garbero.itb@gmail.com>
2022-05-18 15:00:25 -0500

    passed tests

--- index.js
+++ index.js
@@ -11,6 +11,84 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   newTutorials = tutorials.map((cb) => {
+//     return Object.assign({}, line)
+//   })
+  
+// }
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.toLowerCase().split(' ').map(function(word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+//   return newTutorials
+// }
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map()
+// }
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map( function(txt) {
+//     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//   })
+//   return newTutorials
+// } GOOD ONE
+
+// String.prototype.toProperCase = function () {
+//   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+// };
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map.toProperCase
+//   return newTutorials
+// }
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str => str.split(' ').map(w => w[0].toUpperCase() + w.substr(1)).join(' '))
 }
+
+// function titleCased(array) {
+//   let titleCasedArray = array.map(x => x.split(' ')
+//   .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+//   .join(' '))
+//   return titleCasedArray
+// }
+// console.log(titleCased(tutorials))
+
+// const titleCased = () => {
+//   return tutorials.map(str => str.toUpperCase())
+// }
+
+// const titleCased = () => {
+//   return tutorials.map(str => function(str) {
+//     return str.toLowerCase().split(' ').map(function (word) {
+//       return (word.charAt(0).toUpperCase() + word.slice(1));
+//     }).join(' ');
+//   }) 
+// }
+
+// const titleCased = () => {
+//   return tutorials.map(str => str.toLowerCase().split(' ')).map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//   }
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map(array => function(array) {
+//     return array.toLowerCase().split(' ').map(function (word) {
+//       return (word.charAt(0).toUpperCase() + word.slice(1));
+//     }).join(' ');
+//   }) 
+//   return newTutorials
+// }
+
+
+
+
+// return tutorials
+
